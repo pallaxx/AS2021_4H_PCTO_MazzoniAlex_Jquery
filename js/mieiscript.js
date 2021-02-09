4447639a8311e6896b907ca14d516e1b6636df7e
pallaxx <alexmaz03@hotmail.it>
2021-02-09 18:59:30 +0100

    Secondo commit

--- js/mieiscript.js
+++ js/mieiscript.js
@@ -6,60 +6,65 @@ jQuery
         $('#myInput').trigger('focus')
     }
 );
-var i=0, flag=0, testo;
+
+var i=0, m=0;
 var CognomeNomeVerifica=[];
 
 jQuery("#miobottone").on("click",function() 
-    {
+{
     var nome=jQuery("#nome").val();
     var cognome=jQuery("#cognome").val();
 
-    $('#messaggino').click();
-    jQuery("#staticBackdropLabel").html("Nominativo inserito!");
-    var testo=("Hai inserito il nominativo "+ nome + " " + cognome+"<br> confermare?");;
-    jQuery("#corpomessagino").html(testo);
-    jQuery("#bottoneprimario").removeAttr("hidden");
-    jQuery("#bottonesecondario").html("Chiudi");
-    jQuery("#bottoneprimario").html("Confermare!");
-        $("#bottoneprimario").on("click",
-            function() 
+    CognomeNomeVerifica[i]=nome + " " + cognome;
+    flag=0;
+    if(i>=1)
+    {
+        var f=0;
+        for(var j=0; j<i;j++)
+        {
+            if(CognomeNomeVerifica[j]==CognomeNomeVerifica[i])
             {
-
-                CognomeNomeVerifica[i]=nome+" "+cognome;
-
-                    if(i>=1)
-                    {
-                        for(var j=0; j<i;j++)
-                        {
-                            if(CognomeNomeVerifica[j]==CognomeNomeVerifica[i])
-                            {
-                                testo=("Hai inserito un altro "+ CognomeNomeVerifica[i]);
-                                flag=1;
-                                i--;
-
-                                //il modal funziona ma scompare subito
-                                
-                                $('#messaggino').click();         
-                                jQuery('#bottoneprimario').prop("hidden", true);
-                                jQuery("#staticBackdropLabel").html("Attenzione!")
-                                jQuery("#corpomessagino").html(t)
-                                jQuery("#bottonesecondario").html("Ho capito!")
-                            }
-                        }
-                    }
-
-                    if(flag==0)
-                    {
-                        var blocco=jQuery("<div><p>"+CognomeNomeVerifica[i]+"</p></div>");
-                        blocco.addClass("blocconomi");
-                        blocco.appendTo("#nomi");
-                    }
-
-
-                    i++;
-                    }
-                );
+                f=1;
+                testo=("Hai inserito un altro "+ CognomeNomeVerifica[i]);
+                flag=1;
+                    
+                //il modal funziona ma scompare subito
+                    
+                $('#messaggino').click();         
+                jQuery('#bottoneprimario').prop("hidden", true);
+                jQuery("#staticBackdropLabel").html("Attenzione!");
+                jQuery("#corpomessagino").html(testo);     
+                jQuery("#bottonesecondario").html("Ho capito!");
             }
-        );
+        }
+        if(f==0)
+        {
+            m++;
+        }
+    
     }
-);
+    if(flag==0)
+    {
+        $('#messaggino').click();
+        jQuery("#staticBackdropLabel").html("Nominativo inserito!");
+        var testo=("Hai inserito il nominativo "+ nome + " " + cognome+"<br> confermare?");;
+        jQuery("#corpomessagino").html(testo);
+        jQuery("#bottoneprimario").removeAttr("hidden");
+        jQuery("#bottonesecondario").html("Chiudi");
+        jQuery("#bottoneprimario").html("Confermare!");
+
+        $("#bottoneprimario").on("click",
+        function() 
+        {
+            if(m==i)
+            {
+                var blocco=jQuery("<div><p>"+CognomeNomeVerifica[i]+"</p></div>");
+                blocco.addClass("blocconomi");
+                blocco.appendTo("#nomi");
+                i++;
+            }
+        });
+
+    }
+});
+});
